81a7004c5697b1dc14e9a30b2e52e88c00d77813
Eric Crudup <briefcasemanx@gmail.com>
2022-03-25 10:49:22 -0700

    adds definition, can now interact with database

---
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace PierresTreats.Models
+{
+  public class PierresTreatsContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<FlavorTreat> FlavorTreats { get; set; }
+
+    public PierresTreatsContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
